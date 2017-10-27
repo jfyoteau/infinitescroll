4948ca52ff2a0ae0b251e9ae4cd88db4042e61f0
ヨト・ジャンフランソワ <jf.yoteau@yoto-no-MacBook-Pro.local>
2017-10-28 08:55:45 +0900

    Initial Commit

---
+++ sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by ヨト　ジャンフランソワ on 28/10/2017.
+//  Copyright © 2017 tcmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
